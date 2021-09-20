6bc5b80b515e2717c1fd3337a13260a1f8d9310a
khw970421 <khw970421@kakao.com>
2021-09-20 16:50:50 +0900

    화살표오류 제거

---
+++ vue_start/vueplugin/src/components/Count.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <div>{{counter}}</div>
+    <button v-on:click="increase">Click me!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods : {
+    increase(){
+      this.counter += 1;
+    }
+  }
+}
+</script>
