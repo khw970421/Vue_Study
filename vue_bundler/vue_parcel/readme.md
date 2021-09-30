220322f18150ce38a2968dc2c1203df15c732034
khw970421 <khw970421@kakao.com>
2021-09-30 18:11:43 +0900

    parcel+vue

---
+++ vue_bundler/vue_parcel/readme.md
@@ -0,0 +1,12 @@
+## 필수로 하는것 
+
+npm init -y
+npm i vue@next
+npm i -D parcel
+npm i -D sass
+
+## 선택으로 하는 것 
+npm run dev
+npm run build
+
+
---
+++ vue_bundler/vue_parcel/src/main.js
@@ -0,0 +1,6 @@
+// 상대경로를 입력하지 않을 경우 node_modules에 설치된 것이다.
+import * as Vue from 'vue'
+
+import App from './App.vue'
+
+Vue.createApp(App).mount('#app')
